cf030daa83d6d67055b1a7305c618ec0e26d0e0a
cgllucasfernandes-stack <cgl.lucasfernandes@gmail.com>
2026-02-06 10:41:38 -0300

    Update __manifest__.py

--- Projeto_odoo/fleet_vehicle_reservation/__manifest__.py
+++ Projeto_odoo/fleet_vehicle_reservation/__manifest__.py
@@ -1,10 +1,12 @@
 {
+    #Nome do módulo na interface do Odoo
     'name': 'Reserva de Veículos da Frota',
     'version': '1.0',
     'category': 'Human Resources/Fleet',
     'summary': 'Módulo para reserva de veículos da frota por funcionários',
+    #Licença padrão para módulos comunitários
     'license': 'LGPL-3',
-    'author': 'Seu Nome',
+    'author': 'Lucas Fernandes',
     'depends': [
         'fleet', 
         'hr', 
@@ -12,11 +14,10 @@
     ],
     'data': [
         'security/ir.model.access.csv',
-        #'data/sequence.xml',
-        #'views/fleet_reservation_views.xml',
-        #'views/fleet_reservation_menus.xml',
+        #'data/sequence.xml', houve algum erro na qual eu não sei identificar, por isso não consigo utlizar o 'sequence.xml' aqui
+        'views/fleet_reservation_views.xml',
+        #'views/fleet_reservation_menus.xml', Aconteceu o mesmo caso aqui, mas eu deixei os codigos deles no diretório caso queira 
     ],
     'installable': True,
     'application': True,
-    'license': 'LGPL-3',
 }

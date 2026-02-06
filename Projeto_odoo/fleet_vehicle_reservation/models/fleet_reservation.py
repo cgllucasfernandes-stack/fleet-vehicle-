917b68c0087664b17caf6747d28abd2845e8fd4b
cgllucasfernandes-stack <cgl.lucasfernandes@gmail.com>
2026-02-06 14:06:09 -0300

    Update fleet_reservation.py

--- Projeto_odoo/fleet_vehicle_reservation/models/fleet_reservation.py
+++ Projeto_odoo/fleet_vehicle_reservation/models/fleet_reservation.py
@@ -4,6 +4,7 @@ from odoo.exceptions import ValidationError
 class FleetVehicleReservation(models.Model):
     _name = 'fleet.vehicle.reservation'
     _description = 'Reserva de Veículo'
+    # Herança dos modelos de mensagem e atividades para abilitar o Chatter
     _inherit = ['mail.thread', 'mail.activity.mixin'] # Adiciona o Chatter (opcional/recomendado)
     
 
@@ -14,21 +15,26 @@ class FleetVehicleReservation(models.Model):
         copy=False, 
         readonly=True, 
         index=True, 
-        default=lambda self: _('Novo')
+        default=lambda self: _('Novo')# Rastreamento de mudanças no chatter
     )
+    # Relacionamento com o modelo de veículos da frota nativo do odoo
     vehicle_id = fields.Many2one('fleet.vehicle', string='Veículo', required=True, tracking=True)
+    # Relacionamento com o modelo de funcionários (HR) nativo odoo
     employee_id = fields.Many2one('hr.employee', string='Funcionário', required=True, tracking=True)
     start_datetime = fields.Datetime(string='Início da Reserva', required=True, tracking=True)
     end_datetime = fields.Datetime(string='Fim da Reserva', required=True, tracking=True)
     purpose = fields.Text(string='Finalidade')
     work_order_ref = fields.Char(string='Ref. Ordem de Trabalho')
-    
+
+    # Maquina de eatados, contra o fluxo de reserva
     state = fields.Selection([
         ('draft', 'Rascunho'),
         ('confirmed', 'Confirmado'),
         ('cancel', 'Cancelado'),
     ], string='Status', default='draft', tracking=True)
 
+
+    # Metodos ORM
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
@@ -37,7 +43,7 @@ class FleetVehicleReservation(models.Model):
                 vals['name'] = self.env['ir.sequence'].next_by_code('fleet.vehicle.reservation') or _('Novo')
         return super(FleetVehicleReservation, self).create(vals_list)
 
-    # --- Lógica de Validação (O que você aprendeu) ---
+    # Regras de negócio e validações
     @api.constrains('vehicle_id', 'start_datetime', 'end_datetime', 'state')
     def _check_date_overlap(self):
         for record in self:
@@ -45,13 +51,13 @@ class FleetVehicleReservation(models.Model):
             if record.state != 'confirmed':
                 continue
 
-            # Validação básica de integridade
+            # Data final não pode ser menor que a inicial
             if record.end_datetime <= record.start_datetime:
                 raise ValidationError(_("A data de fim deve ser posterior à data de início."))
 
             # A Regra Matemática de Conflito: (Início A < Fim B) E (Fim A > Início B)
             domain = [
-                ('id', '!=', record.id), # Não comparar a reserva com ela mesma
+                ('id', '!=', record.id), # Exclui o proprio registro de busca
                 ('vehicle_id', '=', record.vehicle_id.id),
                 ('state', '=', 'confirmed'),
                 ('start_datetime', '<', record.end_datetime),
@@ -65,7 +71,7 @@ class FleetVehicleReservation(models.Model):
                     "para o período de %s até %s (Referência: %s)."
                 ) % (record.vehicle_id.name, overlap.start_datetime, overlap.end_datetime, overlap.name))
 
-    # --- Ações de Botão ---
+    # srsrssrsssrsAções de Botão 
     def action_confirm(self):
         self.write({'state': 'confirmed'})
 
